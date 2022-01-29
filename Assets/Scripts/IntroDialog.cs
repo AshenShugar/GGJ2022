34171b124fe13748860e640f15a10f3ec500ffae
betterwithlasers <fa_767@hotmail.com>
2022-01-30 10:08:18 +1100

    Slight tweak to intro cutscene

--- Assets/Scripts/IntroDialog.cs
+++ Assets/Scripts/IntroDialog.cs
@@ -42,7 +42,7 @@ public class IntroDialog : MonoBehaviour
             yield return new WaitForSeconds(typingSpeed);
         }
 
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(1.5f);
 
         textDisplayAnim.SetTrigger("TextChange");
 
