829a619b64bf44833efbb7b4aeb045f225aa46aa
Peter J Armstrong <ashen_s@yahoo.com.au>
2022-01-30 13:40:08 +1100

    Adding music/sfx Menu buttons now have click sfx

--- Assets/Scripts/MenuControls.cs
+++ Assets/Scripts/MenuControls.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MenuControls : MonoBehaviour {
 	public string SceneToPlayName = "IntroCutscene";
+	[SerializeField]
+	private AudioSource _as;
 
 	// Start is called before the first frame update
 	void Start ()
@@ -12,19 +14,31 @@ public class MenuControls : MonoBehaviour {
 
 	}
 
+	private IEnumerator SceneChange ()
+	{
+		yield return new WaitForSeconds (_as.clip.length);
+		SceneManager.LoadScene (SceneToPlayName);
+	}
+
 	public void PlayButton ()
 	{
-		SceneManager.LoadScene (SceneToPlayName);
+		StartCoroutine (SceneChange ());
 	}
 
 	public void QuitButton ()
 	{
+		StartCoroutine (QuitAction ());
+	}
+
+	private IEnumerator QuitAction()
+	{
+		yield return new WaitForSeconds (_as.clip.length);
+
 #if UNITY_EDITOR
 		UnityEditor.EditorApplication.isPlaying = false;
 #else
 		Application.Quit ();
 #endif
-
 	}
 
     // Update is called once per frame
