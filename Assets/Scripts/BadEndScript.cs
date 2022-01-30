ac92ebd877ee3e93535100e4708bac42e4e7dc93
Peter J Armstrong <ashen_s@yahoo.com.au>
2022-01-30 11:55:55 +1100

    BadEnd scene now with poster, music fades in somewhat.

--- Assets/Scripts/BadEndScript.cs
+++ Assets/Scripts/BadEndScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BadEndScript : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class BadEndScript : MonoBehaviour
 	private float FadeInTime = 4.0f;
 	[SerializeField]
 	private string MenuSceneName = "MenuScene";
+	[SerializeField]
+	private Image Img;
 
     // Start is called before the first frame update
     void Start()
@@ -27,16 +30,14 @@ public class BadEndScript : MonoBehaviour
 	private IEnumerator SoundPlaying ()
 	{
 		
-		/* Was thinking about trying to increase the volume as it plays
 		float timer = 0;
 		while (timer < _as.clip.length) {
 			timer += Time.deltaTime;
-			_as.volume *= 1.1f;
+			_as.volume = 0.25f + (0.75f * (timer / _as.clip.length));
 			yield return null;
 		}
-		*/
 
-		yield return new WaitForSeconds (_as.clip.length);
+		// yield return new WaitForSeconds (_as.clip.length);
 
 		StartCoroutine (FadeIn ());
 
@@ -45,9 +46,13 @@ public class BadEndScript : MonoBehaviour
 	private IEnumerator FadeIn ()
 	{
 		float timer = 0;
+		Color tmpColour = Img.color;
 		while (timer < FadeInTime) {
 			timer += Time.deltaTime;
 			// Do fade in some more.
+			tmpColour.a = timer / (FadeInTime * 0.5f);
+			Img.color = tmpColour;
+
 			yield return null;
 		}
 		SceneManager.LoadScene (MenuSceneName);
